1ffc2f057b0c73b14d8f1e73aca8c00e897bfdec
edsown <edsonoliveira.contato@gmail.com>
2022-01-28 17:16:21 -0300

    adjusts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
 import items from "./Data";
 import Menu from "./Menu";
-import Categories from "./Categories";
+
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Contact from "./Contact";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categoryItems, setCategoryItems] = useState([]);
+  const [menuItems] = useState(items);
 
   return (
     <Router>
